fd650dbb3e3bc1d47345a041b54ecffac813988d
josemaria9 <josemaria070500@gmail.com>
2021-02-05 09:53:30 +0100

    Inicio

---
+++ examen2ev/exbutton/exbutton.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-exbutton',
+  templateUrl: './exbutton.component.html',
+  styleUrls: ['./exbutton.component.css']
+})
+export class EXButtonComponent {
+
+}
---
+++ examen2ev/exsecond-view/exsecond-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exsecond-view',
+  templateUrl: './exsecond-view.component.html',
+  styleUrls: ['./exsecond-view.component.css']
+})
+export class EXSecondViewComponent {
+
+}
